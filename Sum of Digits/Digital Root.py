803756a71869f1391b2b49df801b9e7a7e74173b
guilherme <guilherme.kaua.matos@gmail.com>
2025-10-16 16:18:17 -0300

    melhorei mais

--- Sum of Digits/Digital Root.py
+++ Sum of Digits/Digital Root.py
@@ -5,18 +5,10 @@
 # A entrada será um número inteiro não negativo.
 
 def digital_root(n):
-    n = str(n)
-    soma = 0
-
-    while len(n) > 1:
+    while n >= 10:
         soma = 0
-        for i in range(len(n)):
-            soma += int(n[i])
-        n = str(soma)   
-    return soma    
-
-
-print(digital_root(16)) # 1 + 6 = 7
-print(digital_root(942)) # 9 + 4 + 2 = 15 -> 1 + 5 = 6
-print(digital_root(132189)) # 1 + 3 + 2 + 1 + 8 + 9 = 24 -> 2 + 4 = 6
-print(digital_root(493193)) # 4 + 9 + 3 + 1 + 9 + 3 = 29 -> 2 + 9 = 11 -> 1 + 1 = 2      
+        for i in str(n):
+            soma += int(i)
+        n = soma
+    if n < 10:
+        return n   
